5fb51e6187c8eb7a1cc9ab7a26a70cb13f561193
chen11223347 <242318605@qq.com>
2026-06-27 13:21:59 +0800

    创建 dump.cs

---
+++ dump.cs
@@ -0,0 +1,11 @@
+	public System.Void CommanderModeRefreshNode(); // 0x6880db4  //  指挥官模式刷新节点
+	
+	public static System.Void StartGuideLogicType2(Assets.Scripts.BusinessComponent.CGuideTipsFormViewViewData viewData); // 0x96685b0  //  入门指南逻辑类型2
+	
+	public System.UInt32 GetUsingSoldierSkinId(System.Boolean includeShare); // 0x62e4a10  //  使用士兵皮肤 ID
+	
+	private static System.UInt32 GetBattleUISkinId(Assets.Scripts.GameLogic.Player player, out System.Boolean& isUISkin); // 0x6890ca4  //  获取战斗 UISkin Id
+	
+		public System.Void InitAIHeroListByBranch(System.Boolean useOfflineHeroConfig); // 0xa2092d8  //  按分支初始化 AIHero 列表
+		
+			public static System.UInt32 GetCareerTitleShowID(); // 0xac46e24  //  获取职业头衔显示 ID
